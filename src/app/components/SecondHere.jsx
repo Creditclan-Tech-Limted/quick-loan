f8733a9fadd81ac9a8a2a6552dfa74d469dff4fe
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-07-25 11:17:58 +0100

    feat: Initial Update

---
+++ src/app/components/SecondHere.jsx
@@ -0,0 +1,42 @@
+'use client'
+import React, { useEffect, useRef } from 'react'
+import Button from './global/Button';
+
+const SecondHere = ({ referral_code }) => {
+  const widget = useRef();
+
+  const handleOpenWidget = async () => {
+    try {
+      widget.current.open()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const CreditClan = window.CreditClan;
+    widget.current = CreditClan?.init('2pXrz4qbDmTRH7ruHxwlY3Jy1N03Db80zCu6AXZG5stOQFwgEN9K5MI1a7joyTBq', { class: 'ccopen', ref: referral_code })
+  }, [])
+  return (
+    <>
+      <div class="h-screen w-full py-6 rounded-lg shadow pb-0 relative flex flex-col">
+        <div class="bg-responsiveHero bg-cover absolute inset-0 h- w-full"></div>
+        <div className="container max-w-7xl mx-auto z-10 relative pl-5 md:pl-[40px]">
+          <img src="https://clan.africa/images/clan-light.png" alt="" className='w-24 z-10' />
+        </div>
+        <div className='text-white relative max-w-7xl mx-auto my-auto'>
+          <div className='text-7xl md:text-8xl font-bold text-left md:text-center px-5 md:px-0 leading-1'>
+            Get up to <span className='text-yellow-500'> 5 Million</span> Loan in less than 3 minutes
+          </div>
+          <div className="flex my-10">
+            <Button className='mx-5 md:mx-auto' variant='filled' color='white' size='xl' onClick={handleOpenWidget}>
+              Get started
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SecondHere;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
+'use client'
 import Hero from './components/Hero'
+import SecondHere from './components/SecondHere';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  const query = useSearchParams().get('r');
+
+  console.log({ query });
   return (
     <>
-      <Hero />
+      <SecondHere referral_code={query} />
+      {/* <Hero /> */}
     </>
   )
 }
