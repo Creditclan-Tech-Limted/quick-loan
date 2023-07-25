f483766d18c3a4aff4b45a04dcdfb79a9cf29c0b
Jude <judecodes@gmail.com>
2023-07-25 15:26:37 +0100

    minor refactoring

--- src/app/components/SecondHere.jsx
+++ src/app/components/SecondHere.jsx
@@ -1,23 +1,27 @@
-'use client'
-import React, { useEffect, useRef } from 'react'
+'use client';
+import React, { useEffect, useRef } from 'react';
 import Button from './global/Button';
 
+const CreditClan = window.CreditClan;
+
 const SecondHere = ({ referral_code }) => {
-  console.log({ referral_code });
-  const widget = useRef();
+  const widget = useRef(null);
 
   const handleOpenWidget = async () => {
     try {
-      widget.current.open()
+      widget.current.open();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    const CreditClan = window.CreditClan;
-    widget.current = CreditClan?.init('2pXrz4qbDmTRH7ruHxwlY3Jy1N03Db80zCu6AXZG5stOQFwgEN9K5MI1a7joyTBq', { class: 'ccopen', ref: referral_code })
-  }, [])
+    widget.current = CreditClan?.init('2pXrz4qbDmTRH7ruHxwlY3Jy1N03Db80zCu6AXZG5stOQFwgEN9K5MI1a7joyTBq', {
+      class: 'ccopen',
+      ref: referral_code
+    });
+  }, []);
+
   return (
     <>
       <div className="h-screen w-full py-6 rounded-lg shadow pb-0 relative flex flex-col">
@@ -30,14 +34,14 @@ const SecondHere = ({ referral_code }) => {
             Get up to <span className='text-yellow-500'> 5 Million</span> Loan in less than 3 minutes
           </div>
           <div className="flex my-10">
-            <Button className='mx-5 md:mx-auto' variant='filled' color='white' size='xl' onClick={handleOpenWidget}>
+            <Button className='mx-5 md:mx-auto' variant='filled' color='white' size='xl' onClick={ handleOpenWidget }>
               Get started
             </Button>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default SecondHere;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,14 @@
-'use client'
-import Hero from './components/Hero'
+'use client';
+import Hero from './components/Hero';
 import SecondHere from './components/SecondHere';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Home() {
   const query = useSearchParams().get('r');
 
-  console.log({ query });
   return (
     <>
-      <SecondHere referral_code={query} />
-      {/* <Hero /> */}
+      <SecondHere referral_code={ query } />
     </>
-  )
+  );
 }
